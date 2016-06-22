56acfca48c4a207ca1c76dff96330ceba5817160
Travis CI <jack.humb+travis.ci@gmail.com>
2016-06-22 16:02:55 +0000

    complete keyboards and compiled files from qmk_firmware/subprojects@25a3f7e

---
+++ keyboard/clueboard/clueboard.c
@@ -0,0 +1,11 @@
+#include "clueboard.h"
+
+__attribute__ ((weak))
+void matrix_init_user(void) {
+	// leave these blank
+}
+
+__attribute__ ((weak))
+void matrix_scan_user(void) {
+	// leave these blank
+}
