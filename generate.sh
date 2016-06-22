dc66e1fc10ff80ad78a879694ff67a7fdebd3e2e
Jack Humbert <jack.humb@gmail.com>
2016-06-22 17:54:02 -0400

    add download and view buttons

--- generate.sh
+++ generate.sh
@@ -5,11 +5,21 @@ pandoc https://raw.githubusercontent.com/jackhumbert/qmk_firmware/master/README.
 --template standalone.html --toc --toc-depth 3
 
 # for i in $(find . -name "README.md" -o -name "readme.md"); do MARKDOWN=$(<$i) mo/mo templates/readme.mo > ${i%/*}/index.html; done;
-for i in $(find . -name "README.md" -o -name "readme.md"); do \
-pandoc $i \
--f markdown_github --to html --standalone --output ${i%/*}/index.html \
---template standalone.html --toc --toc-depth 3 --variable keyboard=$(echo $i | sed -e 's/.*\/keyboard\/\([^\/]*\).*/\1/') \
-; done;
+for i in $(find . -name "README.md" -o -name "readme.md"); do
+	if [[ $i == *"keymaps"* ]]
+	then
+		KEYMAP_URL=$(echo $i | sed -e 's/.*\/\(keyboard\/[^\/]*\/keymaps\/[^\/]*\).*/\1/')
+	 	KEYMAP=http://github.com/jackhumbert/qmk_firmware/blob/master/${KEYMAP_URL}/keymap.c
+		pandoc $i \
+		-f markdown_github --to html --standalone --output ${i%/*}/index.html \
+		--template standalone.html --toc --toc-depth 3 --variable keyboard=$(echo $i | sed -e 's/.*\/keyboard\/\([^\/]*\).*/\1/') \
+		--variable keymap=$KEYMAP
+	else
+		pandoc $i \
+		-f markdown_github --to html --standalone --output ${i%/*}/index.html \
+		--template standalone.html --toc --toc-depth 3 --variable keyboard=$(echo $i | sed -e 's/.*\/keyboard\/\([^\/]*\).*/\1/')
+	fi
+done
 
 export KEYBOARDS=( $(for i in $(ls -d ./keyboard/*/); do echo $i | sed -e 's/.*\/keyboard\/\([^\/]*\).*/\1/'; done) )
 . mo/mo
