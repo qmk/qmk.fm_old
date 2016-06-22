a8c83fd18d35dacc703c56af2bf310cd022f77c9
Jack Humbert <jack.humb@gmail.com>
2016-06-22 15:19:51 -0400

    pandoc to generate core readme automatically

--- generate.sh
+++ generate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-export KEYBOARDS=( $(ls keyboard) )
-. mo/mo
-mo templates/index.mo > index.html
-
-
-
+# export KEYBOARDS=( $(ls keyboard) )
+# . mo/mo
+# mo templates/index.mo > index.html
+pandoc https://raw.githubusercontent.com/jackhumbert/qmk_firmware/master/README.md \
+-f markdown_github --to html --standalone --output index.html \
+--template standalone.html --toc --toc-depth 3
 
 # for i in $(find . -name "README.md" -o -name "readme.md"); do MARKDOWN=$(<$i) mo/mo templates/readme.mo > ${i%/*}/index.html; done;
+# for i in $(find . -name "README.md" -o -name "readme.md"); do pandoc --from markdown_github --to html --standalone $i --output ${i%/*}/index.html --template templates/template.html --css templates/template.css --self-contained --toc --toc-depth 2; done;
--- templates/readme.mo
+++ templates/readme.mo
@@ -2,9 +2,9 @@
 <html>
 <title>QMK Firmware</title>
 
-<xmp theme="united" style="display:none;">
+
 {{MARKDOWN}}
-</xmp>
+
 
 <script src="http://strapdownjs.com/v/0.2/strapdown.js"></script>
 </html>
