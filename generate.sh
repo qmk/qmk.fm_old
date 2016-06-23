6621bbaf64edce32889ed97572513aba1eaf37ff
Jack Humbert <jack.humb@gmail.com>
2016-06-23 00:27:41 -0400

    updates to keyboards

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ pandoc https://raw.githubusercontent.com/jackhumbert/qmk_firmware/master/README.
 for i in $(find . -name "README.md" -o -name "readme.md"); do
 	if [[ $i == *"keymaps"* ]]
 	then
-		KEYMAP_URL=$(echo $i | sed -e 's/.*\/\(keyboard\/[^\/]*\/keymaps\/[^\/]*\).*/\1/')
+		KEYMAP_URL=$(echo $i | sed -e 's/.*\/\(keyboards\/[^\/]*\/keymaps\/[^\/]*\).*/\1/')
 	 	KEYMAP_LINK=http://github.com/jackhumbert/qmk_firmware/blob/master/${KEYMAP_URL}/keymap.c
 
 	 	if [ -f ${KEYMAP_URL}/config.h ]
@@ -33,24 +33,24 @@ for i in $(find . -name "README.md" -o -name "readme.md"); do
 
 		cat $i ${KEYMAP_URL}/Makefile.md ${KEYMAP_URL}/config.h.md | pandoc \
 		-f markdown_github --to html --standalone --output ${i%/*}/index.html \
-		--template standalone.html --toc --toc-depth 3 --variable keyboard=$(echo $i | sed -e 's/.*\/keyboard\/\([^\/]*\).*/\1/') \
+		--template standalone.html --toc --toc-depth 3 --variable keyboard=$(echo $i | sed -e 's/.*\/keyboards\/\([^\/]*\).*/\1/') \
 		--variable keymap_link=$KEYMAP_LINK --variable keymap=$(echo $i | sed -e 's/.*\/keymaps\/\([^\/]*\).*/\1/')
-	elif [[ $i == *"/keyboard/"* ]]
+	elif [[ $i == *"/keyboards/"* ]]
 	then
-		KEYBOARD_URL=$(echo $i | sed -e 's/.*\/\(keyboard\/[^\/]*\).*/\1/')
+		KEYBOARD_URL=$(echo $i | sed -e 's/.*\/\(keyboards\/[^\/]*\).*/\1/')
 		export KEYMAPS=( $(for i in $(ls -d ${KEYBOARD_URL}/keymaps/*/); do echo $i | sed -e 's/.*\/keymaps\/\([^\/]*\).*/\1/'; done) )
 		. mo/mo
 		mo templates/keymaps.mo > ${KEYBOARD_URL}/keymaps.md
 		pandoc $i \
 		-f markdown_github --to html --standalone --output ${i%/*}/index.html \
-		--template standalone.html --toc --toc-depth 3 --variable keyboard=$(echo $i | sed -e 's/.*\/keyboard\/\([^\/]*\).*/\1/') \
+		--template standalone.html --toc --toc-depth 3 --variable keyboard=$(echo $i | sed -e 's/.*\/keyboards\/\([^\/]*\).*/\1/') \
 		--variable keymaps="$(pandoc ${KEYBOARD_URL}/keymaps.md)"
 	fi
 done
 
-export KEYBOARDS=( $(for i in $(ls -d ./keyboard/*/); do echo $i | sed -e 's/.*\/keyboard\/\([^\/]*\).*/\1/'; done) )
+export KEYBOARDS=( $(for i in $(ls -d ./keyboards/*/); do echo $i | sed -e 's/.*\/keyboards\/\([^\/]*\).*/\1/'; done) )
 . mo/mo
 mo templates/keyboards.mo > keyboards.md
 pandoc keyboards.md \
--f markdown_github --to html --standalone --output keyboard/index.html \
+-f markdown_github --to html --standalone --output keyboards/index.html \
 --template standalone.html --toc --toc-depth 3
