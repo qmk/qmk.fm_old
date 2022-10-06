87e0a13a28a12bb48689f2e8bff593ba06a53f60
Karey Higuera <karey.higuera@gmail.com>
2022-10-06 08:02:13 -0400

    update templates for english and spanish

---
+++ _util/_i18n/es/keyboards_template.md
@@ -0,0 +1,27 @@
+---
+title: "Todos los teclados con soporte"
+layout: qmk-editable
+permalink: /es/keyboards/
+lang: es
+# esta página se genera automáticamente
+---
+## Todos los teclados con soporte
+
+Esta página se genera automáticamente de los teclados actualmente en QMK. Los archivos .hex predeterminados están actualizados con la última version de QMK, y se publica con cada compilación via Github Actions.
+
+¿Quieres una página de QMK? [Crea un archivo `<keyboard>.md` aquí](https://github.com/qmk/qmk.fm/tree/gh-pages/_pages/keyboards)! (usa otros como ejemplos/patrones)
+
+Actualmente se proveen archivos .hex, .bin, y .uf2 - algunos de los enumerados aquí tal vez no sean lo que se necesita para flashear el teclado. Si es así, [por favor abre una propuesta](https://github.com/qmk/qmk.fm/issues)!
+
+<style>
+.fa.psi-icon {
+    font-size: 14px;
+    font-family: sans-serif;
+}
+</style>
+
+| Carpeta de QMK | firmware predeterminado | página de qmk.fm |
+| --- | --- | --- |
+{% for keyboard in keyboards -%}
+    | <i class='fa fa-github' aria-hidden='true'></i> [{{keyboard['pathname']}}](https://github.com/qmk/qmk_firmware/tree/master/keyboards/{{keyboard['pathname']}}) | [<i class='fa fa-download' aria-hidden='true'></i>](https://qmk.fm/{{keyboard['filename']}} "último firmware predeterminado de QMK para {{keyboard['pathname']}}")[<span class='fa-stack fa-lg'><i class='fa fa-circle fa-stack-1x'></i><i class='fa fa-inverse fa-stack-1x psi-icon'>&Psi;</i></span>](qmk:https://qmk.fm/{{keyboard['filename']}} \"Abrir en QMK Toolbox\") | {% if keyboard['readme'] %} [{{keyboard['name']}}](http://qmk.fm/es/keyboards/{{keyboard['pathname']}}/) {% endif %}|
+{% endfor -%}
--- _util/generate_keyboard_page.py
+++ _util/generate_keyboard_page.py
@@ -4,65 +4,47 @@ import os
 
 from jinja2 import Template
 
-pagetext = """
----
-title: "All Supported Keyboards"
-layout: qmk-editable
-permalink: /keyboards/
-# this page is automatically generated
----
-## All Supported Keyboards
-
-This page is automatically generated from keyboards currently in QMK. Default .hex files are up-to-date with the latest version of QMK, and get pushed every build via Travis.
-
-Want a qmk.fm page? [Create a `<keyboard>.md` file for it here](https://github.com/qmk/qmk.fm/tree/gh-pages/_pages/keyboards)! (use others as examples/templates)
-
-Currently only .hex files are provided - some listed here may not be what's needed to flash the keyboard. If this is the case, [please open an issue](https://github.com/qmk/qmk.fm/issues)!
-
-<style>
-.fa.psi-icon {
-    font-size: 14px;
-    font-family: sans-serif;
-}
-</style>
-
-| QMK Folder | default firmware | qmk.fm page |
-| --- | --- | --- |
-{% for keyboard in keyboards -%}
-    | <i class='fa fa-github' aria-hidden='true'></i> [{{keyboard['pathname']}}](https://github.com/qmk/qmk_firmware/tree/master/keyboards/{{keyboard['pathname']}}) | [<i class='fa fa-download' aria-hidden='true'></i>](https://qmk.fm/{{keyboard['filename']}} "{{keyboard['pathname']}}'s latest default QMK firmware")[<span class='fa-stack fa-lg'><i class='fa fa-circle fa-stack-1x'></i><i class='fa fa-inverse fa-stack-1x psi-icon'>&Psi;</i></span>](qmk:https://qmk.fm/{{keyboard['filename']}} \"Open in QMK Toolbox\") | {% if keyboard['readme'] %} [{{keyboard['name']}}](http://qmk.fm/keyboards/{{keyboard['pathname']}}/) {% endif %}|
-{% endfor -%}
-"""
-
 with open('.keyboards', 'r') as fd:
     keyboards = fd.readlines()
 
-keyboard_entries = []
-for keyboard in keyboards:
-    keyboard = keyboard.strip()
-    keyboard_name = keyboard.replace('/', '_')
-    format = None
-    readme = None
+# loop through each language here
+languages = [item for item in os.listdir('_util/_i18n')]
 
-    if os.path.exists('compiled/%s_default.bin' % keyboard_name):
-        format = 'bin'
-    elif os.path.exists('compiled/%s_default.hex' % keyboard_name):
-        format = 'hex'
+for language in languages:
+  keyboard_entries = []
+  for keyboard in keyboards:
+      keyboard = keyboard.strip()
+      keyboard_base = keyboard.split('/')[0]
+      keyboard_name = keyboard.replace('/', '_')
+      format = None
+      readme = None
 
-    if os.path.exists('_i18n/en/keyboards/%s.md' % keyboard.split('/')[0]):
-        readme = '_i18n/en/keyboards/%s.md' % keyboard.split('/')[0]
+      if os.path.exists(f'compiled/{keyboard_name}_default.bin'):
+          format = 'bin'
+      elif os.path.exists(f'compiled/{keyboard_name}_default.hex'):
+          format = 'hex'
+      elif os.path.exists(f'compiled/{keyboard_name}_default.uf2'):
+          format = 'uf2'
 
-    if format:
-        keyboard_entries.append({
-            'pathname': keyboard,
-            'name': keyboard_name,
-            'filename': 'compiled/%s_default.%s' % (keyboard_name, format),
-            'readme': readme,
-        })
+      if os.path.exists(f'_i18n/{language}/keyboards/{keyboard_base}.md'):
+          readme = f'_i18n/{language}/keyboards/{keyboard_base}.md'
 
-# Write out the generated template
-if os.path.exists('_i18n/en/keyboards.md'):
-    os.remove('_i18n/en/keyboards.md')
+      if format:
+          keyboard_entries.append({
+              'pathname': keyboard,
+              'name': keyboard_name,
+              'filename': f'compiled/{keyboard_name}_default.{format}',
+              'readme': readme,
+          })
 
-with open('_i18n/en/keyboards.md', 'w') as fd:
-    t = Template(pagetext)
-    fd.write(t.render(keyboards=keyboard_entries))
+  # Write out the generated template
+  if os.path.exists(f'_i18n/{language}/keyboards.md'):
+      os.remove(f'_i18n/{language}/keyboards.md')
+
+  with open(f'_util/_i18n/{language}/keyboards_template.md', 'r') as fd:
+    pagetext = fd.read()
+
+  with open(f'_i18n/{language}/keyboards.md', 'w') as fd:
+      t = Template(pagetext)
+      fd.write(t.render(keyboards=keyboard_entries))
+#end loop
